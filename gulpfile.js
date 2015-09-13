49770ac63ec41b18b104c2150a92ad120b890fd0
Chris McLean <havok2905@gmail.com>
2015-09-13 18:41:42 -0400

    got sass compiling working

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,16 @@ var gulp       = require('gulp');
 var browserify = require('browserify');
 var babelify   = require('babelify');
 var source     = require('vinyl-source-stream');
+var sass       = require('gulp-sass');
 
-gulp.task('build', function () {
+gulp.task('sass', function() {
+  gulp.src('./sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('jsx', function () {
   return browserify({entries: './app/app.jsx', extensions: ['.jsx'], debug: true})
     .transform(babelify)
     .bundle()
@@ -11,8 +19,9 @@ gulp.task('build', function () {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('watch', ['build'], function () {
-  gulp.watch('**/*.jsx', ['build']);
+gulp.task('watch', ['jsx', 'sass'], function () {
+  gulp.watch('./app/**/*.jsx', ['jsx']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['watch']);
