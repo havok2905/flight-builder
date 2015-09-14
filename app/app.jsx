9fbc82731604df967f7703fb61601c72b0eff48a
Chris McLean <havok2905@gmail.com>
2015-09-14 00:32:47 -0400

    added about page

--- app/app.jsx
+++ app/app.jsx
@@ -4,10 +4,6 @@ import FlightOrderButton from './components/flight_order_button';
 
 let app = (
   <div>
-    <header>
-      <h1>Flight Builder</h1>
-      <h2>Build The Ideal Flight Of Beers</h2>
-    </header>
     <section className='-column'>
       <h3>Build Your Flight Profile</h3>
       <FlightForm/>
